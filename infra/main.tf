1d285e611cc20a36a9403d0c9d446b49f95b080f
Prasanna429 <prasannaretharekar@gmail.com>
2025-07-24 16:21:33 +0530

    Simplify Terraform config by hardcoding bucket name and removing random_string

--- infra/main.tf
+++ infra/main.tf
@@ -3,14 +3,11 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "tf-cicd-${random_string.suffix.result}"
+  bucket = "tf-cicd-hx4v19lt" # Matches existing bucket
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
+# Remove random_string.suffix to simplify and avoid replacement
+# If you need dynamic names later, we can reintroduce it
 terraform {
   backend "s3" {
     bucket         = "prasanna-terraform-state-864981717146"
